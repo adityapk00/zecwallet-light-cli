0bfa5b925df6e15850e43262046990ee8002d0b5
Aditya Kulkarni <adityapk@gmail.com>
2019-09-16 14:01:37 -0700

    Remove outdated directories

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-BASE_DIR=$(pwd)
-
-cd "$BASE_DIR/zcash-client-backend-wasm"
-wasm-pack build
-cd "$BASE_DIR/zcash-client-sdk-js"
-npm install
-cd "$BASE_DIR/demo-www"
-npm install
--- envoy/envoy.Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM envoyproxy/envoy:v1.10.0
-COPY ./envoy.yaml /etc/envoy/envoy.yaml
-CMD /usr/local/bin/envoy -c /etc/envoy/envoy.yaml
