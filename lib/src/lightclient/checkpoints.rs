adba18e13bd181980af0784f6c548593961f6cd9
Aditya Kulkarni <adityapk@gmail.com>
2019-10-22 21:03:11 -0700

    Add new testnet checkpoint

--- lib/src/lightclient/checkpoints.rs
+++ lib/src/lightclient/checkpoints.rs
@@ -10,6 +10,9 @@ fn get_test_checkpoint(height: u64) ->  Option<(u64, &'static str, &'static str)
     let checkpoints: Vec<(u64, &str, &str)> = vec![
         (600000, "0107385846c7451480912c294b6ce1ee1feba6c2619079fd9104f6e71e4d8fe7",
                  "01690698411e3f8badea7da885e556d7aba365a797e9b20b44ac0946dced14b23c001001ab2a18a5a86aa5d77e43b69071b21770b6fe6b3c26304dcaf7f96c0bb3fed74d000186482712fa0f2e5aa2f2700c4ed49ef360820f323d34e2b447b78df5ec4dfa0401a332e89a21afb073cb1db7d6f07396b56a95e97454b9bca5a63d0ebc575d3a33000000000001c9d3564eff54ebc328eab2e4f1150c3637f4f47516f879a0cfebdf49fe7b1d5201c104705fac60a85596010e41260d07f3a64f38f37a112eaef41cd9d736edc5270145e3d4899fcd7f0f1236ae31eafb3f4b65ad6b11a17eae1729cec09bd3afa01a000000011f8322ef806eb2430dc4a7a41c1b344bea5be946efc7b4349c1c9edb14ff9d39"
+        ),
+        (650000, "003f7e09a357a75c3742af1b7e1189a9038a360cebb9d55e158af94a1c5aa682",
+                 "010113f257f93a40e25cfc8161022f21c06fa2bc7fb03ee9f9399b3b30c636715301ef5b99706e40a19596d758bf7f4fd1b83c3054557bf7fab4801985642c317d41100001b2ad599fd7062af72bea99438dc5d8c3aa66ab52ed7dee3e066c4e762bd4e42b0001599dd114ec6c4c5774929a342d530bf109b131b48db2d20855afa9d37c92d6390000019159393c84b1bf439d142ed2c54ee8d5f7599a8b8f95e4035a75c30b0ec0fa4c0128e3a018bd08b2a98ed8b6995826f5857a9dc2777ce6af86db1ae68b01c3c53d0000000001e3ec5d790cc9acc2586fc6e9ce5aae5f5aba32d33e386165c248c4a03ec8ed670000011f8322ef806eb2430dc4a7a41c1b344bea5be946efc7b4349c1c9edb14ff9d39"
         )
     ];
 
@@ -76,6 +79,8 @@ pub mod tests {
         assert_eq!(get_test_checkpoint(500000), None);
         assert_eq!(get_test_checkpoint(600000).unwrap().0, 600000);
         assert_eq!(get_test_checkpoint(625000).unwrap().0, 600000);
+        assert_eq!(get_test_checkpoint(650000).unwrap().0, 650000);
+        assert_eq!(get_test_checkpoint(655000).unwrap().0, 650000);
 
         assert_eq!(get_main_checkpoint(500000), None);
         assert_eq!(get_main_checkpoint(610000).unwrap().0, 610000);
