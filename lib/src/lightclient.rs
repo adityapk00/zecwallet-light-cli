af0e0b9b2bcc131804d5e940f94d45c6fef74481
Aditya Kulkarni <zcash@adityapk.com>
2019-11-01 07:31:01 -0700

    Create .zcash directory if it doesn't exist

--- lib/src/lightclient.rs
+++ lib/src/lightclient.rs
@@ -121,7 +121,14 @@ impl LightClientConfig {
             };
         }
 
-        zcash_data_location.into_boxed_path()
+        // Create directory if it doesn't exist
+        match std::fs::create_dir_all(zcash_data_location.clone()) {
+            Ok(_) => zcash_data_location.into_boxed_path(),
+            Err(e) => {
+                eprintln!("Couldn't create zcash directory!\n{}", e);
+                panic!("Couldn't create zcash directory!");
+            }
+        }
     }
 
     pub fn get_wallet_path(&self) -> Box<Path> {
