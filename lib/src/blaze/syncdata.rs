e127bc9d3eebee6afa57e27de478eb1f46361d55
Aditya Kulkarni <adityapk@gmail.com>
2021-06-11 10:50:57 -0700

    pretty print progress

--- lib/src/blaze/syncdata.rs
+++ lib/src/blaze/syncdata.rs
@@ -7,12 +7,6 @@ use crate::{lightclient::lightclient_config::LightClientConfig, lightwallet::dat
 use super::{node_and_witness_data::NodeAndWitnessData, nullifier_data::NullifierData, sync_status::SyncStatus};
 
 pub struct BlazeSyncData {
-    sync_id: u32,
-    pub(super) start_block: u64,
-    pub(super) end_block: u64,
-
-    in_progress: bool,
-
     pub(crate) sync_status: Arc<RwLock<SyncStatus>>,
     pub(crate) nullifier_data: NullifierData,
     pub(crate) node_data: NodeAndWitnessData,
@@ -20,37 +14,34 @@ pub struct BlazeSyncData {
 
 impl BlazeSyncData {
     pub fn new(config: &LightClientConfig) -> Self {
-        let sync_status = Arc::new(RwLock::new(SyncStatus::new()));
+        let sync_status = Arc::new(RwLock::new(SyncStatus::default()));
 
         Self {
-            sync_id: 0,
-            start_block: 0,
-            end_block: 0,
-            in_progress: false,
             sync_status: sync_status.clone(),
             nullifier_data: NullifierData::new(),
             node_data: NodeAndWitnessData::new(config, sync_status),
         }
     }
 
-    pub fn earliest_block(&self) -> u64 {
-        self.end_block
-    }
-
     pub async fn setup_for_sync(&mut self, start_block: u64, end_block: u64, existing_blocks: Vec<BlockData>) {
         if start_block < end_block {
             panic!("Blocks should be backwards");
         }
-        self.sync_id += 1;
-        self.start_block = start_block;
-        self.end_block = end_block;
-
-        self.in_progress = true;
 
         // Replace the contents with a new syncstatus, essentially clearing it
-        (*self.sync_status.write().await) = SyncStatus::new();
+        {
+            let mut guard = self.sync_status.write().await;
+            let prev_sync_status = guard.clone();
+            (*guard) = SyncStatus::new_sync(prev_sync_status.sync_id + 1, start_block, end_block);
+        }
 
         self.nullifier_data.setup_sync().await;
         self.node_data.setup_sync(existing_blocks).await;
+    }
+
+    // Finish up the sync
+    pub async fn finish(&self) {
+        self.nullifier_data.finish().await;
+        self.sync_status.write().await.finish();
     }
 }
